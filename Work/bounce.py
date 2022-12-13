0bd44604da51049ef9c358a753606d12da7111e8
Rafael Reyes <raffeee.reyes@gmail.com>
2022-12-14 01:16:09 +0800

    Complete exercises in 1.2 A First Program

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce_count = 1
+bounce_rate = 0.6
+ball_height = 100
+
+while bounce_count <= 10:
+    ball_height *= bounce_rate
+    print(bounce_count, round(ball_height, 4))
+    bounce_count += 1
