923f6a8168a99e217a496a192e6c7bfaa716a106
Rafael Reyes <raffeee.reyes@gmail.com>
2022-12-16 00:27:06 +0800

    feat: portfolio_cost function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,13 +2,33 @@
 #
 # Exercise 1.27
 
-totalcost = 0
+import csv
+import sys
 
-with open('Data/portfolio.csv', 'rt') as file:
-    headers = next(file).split(',')
-    for line in file:
-        row = line.split(',')   
-        shares = int(row[1]) * float(row[2])
-        totalcost += shares
+def portfolio_cost(filename):
+    '''
+    Computes stock portfolio costs from CSV files
+    '''
 
-print(f'Total cost {totalcost}')
+    totalcost = 0
+
+    with open(filename, 'rt') as file:
+        lines = csv.reader(file)
+        headers = next(lines)
+        for line in lines:
+            try:
+                shares = int(line[1]) * float(line[2])
+            except ValueError:
+                print("Skipping empty line")
+                pass
+            totalcost += shares
+
+    return totalcost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
